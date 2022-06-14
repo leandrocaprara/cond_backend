91a74efd7693450374c4e0d6f10946b2c5fa7c6d
Leandro Caprara Costa <lecaprara@gmail.com>
2022-06-14 20:03:01 -0300

    feat/users

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,28 @@
-"""cond_backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+from rest_framework_simplejwt.views import (TokenObtainPairView)
+from rest_framework import routers
+
+from users.urls import users_router
+
+router = routers.DefaultRouter()
+#Users
+router.registry.extend(users_router.registry)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+
+    #General routes
+    path('', include(router.urls)),
+
+    path('docs/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+# Swagger
+# OpenAPI 3 documentation with Swagger UI
+urlpatterns.append(path('schema/', SpectacularAPIView.as_view(urlconf=urlpatterns), name="schema"))
+
+urlpatterns.append(path('docs/', SpectacularSwaggerView.as_view(url_name="schema"),name="swagger-ui"))
