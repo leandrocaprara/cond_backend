d537a6e18eddd6f0da13aab6f114d79b439fb4a0
Leandro Caprara Costa <lecaprara@gmail.com>
2022-10-19 20:38:53 -0300

    fix apartment

--- building/admin.py
+++ building/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+
+from building.models import Apartment, Building, Floor
+
+admin.site.register(Apartment)
+admin.site.register(Building)
+admin.site.register(Floor)
